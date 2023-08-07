98ed579323b28d5ff762069608b5718cb90f7594
Melike Arslan <melike.arslann03@gmail.com>
2023-08-07 11:05:27 +0300

    Add project files.

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task Message(String fromUser, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, message);
+        }
+    }
+}
---
+++ ChatApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();
+
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + ":" + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#txtMsg").val();
+
+    connection.invoke("Message", fromUser, message); // Fonksiyon adını "Message" olarak değiştirdik
+
+});
